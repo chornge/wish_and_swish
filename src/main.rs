38b4d3d6ec7ea954a34fbee355e45a1248b89f10
Christian <Mxlightningx@gmail.com>
2025-04-01 22:35:25 -0500

    Build for ESP32c3

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,13 @@ mod engine;
 use engine::listen_for_keyphrase;
 
 fn main() {
-    listen_for_keyphrase();
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_sys::link_patches();
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    info!("Application running!");
+
+    // listen_for_keyphrase();
 }
