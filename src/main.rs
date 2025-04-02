cda42ef70b26bf62a141eac53b1950c378a509ff
Christian <Mxlightningx@gmail.com>
2025-04-02 00:52:10 -0500

    Target thumbv7em / cortex-m-rt

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
-mod engine;
+#![no_std]
+#![no_main]
 
-use engine::listen_for_keyphrase;
+use cortex_m_rt::entry;
+use panic_halt as _;
 
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_sys::link_patches();
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
-
-    info!("Application running!");
-
-    // listen_for_keyphrase();
+#[entry]
+fn main() -> ! {
+    loop {
+        //
+    }
 }
