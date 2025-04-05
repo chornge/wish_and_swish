ba23a1964ff82d529b61462e80e787406f5d5794
Christian <Mxlightningx@gmail.com>
2025-04-04 20:42:24 -0500

    Revert "Add HAL, Async & Timekeeping (embassy)"
    This reverts commit 711a0e489c7fd1232d223a4d68d07c900f16363c.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
 #![no_std]
 #![no_main]
 
-use defmt::info;
-use defmt_rtt as _;
-use embassy_executor::Spawner;
-use panic_probe as _;
+use cortex_m_rt::entry;
+use panic_halt as _;
 
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    info!("Starting...");
-    
-    #[allow(clippy::empty_loop)]
+#[entry]
+fn main() -> ! {
     loop {
         //
     }
